b6a13d46e40a0140beb6839fc5ed384fd2299531
nmyers217 <nick.b.myers@protonmail.com>
2022-07-19 11:56:08 -0500

    Clean up and optimize the solution

--- src/complete/827.making-a-large-island.ts
+++ src/complete/827.making-a-large-island.ts
@@ -6,107 +6,64 @@
 
 // @lc code=start
 function largestIsland(grid: number[][]): number {
-  const hashCoord = function (x: number, y: number): string {
-    return `(${x}, ${y})`;
-  };
-
-  const [rows, cols] = [grid.length, grid[0].length];
-  const seen = new Set<string>();
-  const islands: Set<string>[] = [];
-
-  const cells = function* (grid: number[][]) {
-    for (let y = 0; y < rows; y++) {
-      for (let x = 0; x < cols; x++) {
-        yield [x, y];
-      }
-    }
-  };
+  const n = grid.length;
+  type Id = number;
+  type Size = number;
+  const islandSizes: Record<Id, Size> = {};
+  const dirs = [
+    [0, -1],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+  ];
 
   const neighbors = function* (x: number, y: number) {
-    const dirs = [
-      [0, -1],
-      [1, 0],
-      [0, 1],
-      [-1, 0],
-    ];
     for (const [dx, dy] of dirs) {
       const [nx, ny] = [x + dx, y + dy];
-      if (ny >= 0 && ny < rows && nx >= 0 && nx < cols) {
+      if (ny >= 0 && ny < n && nx >= 0 && nx < n) {
         yield [nx, ny];
       }
     }
   };
 
-  const bfs = function (x: number, y: number): Set<string> {
-    const result = new Set<string>();
-    const queue = [[x, y]];
-
-    while (queue.length > 0) {
-      const [x, y] = queue.shift()!;
-      const key = hashCoord(x, y);
-
-      if (seen.has(key) || grid[y][x] === 0) {
-        continue;
-      }
-
-      result.add(key);
-      seen.add(key);
-      for (const [nx, ny] of neighbors(x, y)) {
-        queue.push([nx, ny]);
-      }
-    }
-
+  const dfs = function (x: number, y: number, id: Id): Size {
+    grid[y][x] = id;
+    let result = 1;
+    for (const [nx, ny] of neighbors(x, y))
+      if (grid[ny][nx] === 1) result += dfs(nx, ny, id);
     return result;
   };
 
-  let maxSize = 0;
+  const searchIsland = function (x: number, y: number, id: Id): Size {
+    islandSizes[id] = dfs(x, y, id);
+    return islandSizes[id];
+  };
 
-  // Build up all the islands
-  for (const [x, y] of cells(grid)) {
-    if (seen.has(hashCoord(x, y))) {
-      continue;
-    }
+  let result = 0;
+  let curId = 2;
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < n; x++) {
+      if (grid[y][x] === 1) {
+        result = Math.max(result, searchIsland(x, y, curId++));
+      } else if (grid[y][x] === 0) {
+        const islands: Record<Id, Size> = {};
 
-    if (grid[y][x] === 1) {
-      const island = bfs(x, y);
-      maxSize = Math.max(maxSize, island.size);
-      islands.push(island);
-    }
-  }
-
-  // console.log(islands);
-  // console.log(maxSize);
-
-  // Check the zeros to see what they connect
-  for (const [x, y] of cells(grid)) {
-    if (grid[y][x] > 0) {
-      continue;
-    }
-
-    // console.log('Turning on', hashCoord(x, y));
-
-    // console.log('Zero Point:', hashCoord(x, y));
-    let bigIsland = new Set<string>([hashCoord(x, y)]);
-    for (const [nx, ny] of neighbors(x, y)) {
-      // console.log('Neighbor', hashCoord(nx, ny));
-      for (const island of islands) {
-        if (island.has(hashCoord(nx, ny))) {
-          bigIsland = new Set([...bigIsland, ...island]);
-          // console.log(bigIsland);
-          break;
+        for (const [nx, ny] of neighbors(x, y)) {
+          if (grid[ny][nx] === 1) {
+            result = Math.max(result, searchIsland(nx, ny, curId++));
+          }
+          if (grid[ny][nx] > 1) {
+            const id = grid[ny][nx];
+            islands[id] = islandSizes[id];
+          }
         }
+
+        const sizes = Object.values(islands).reduce((a, b) => a + b, 1);
+        result = Math.max(result, sizes);
       }
     }
-
-    maxSize = Math.max(maxSize, bigIsland.size);
   }
 
-  // console.log(maxSize);
-
-  // 1,0,1
-  // 0,0,0
-  // 0,1,1
-
-  return maxSize;
+  return result;
 }
 // @lc code=end
